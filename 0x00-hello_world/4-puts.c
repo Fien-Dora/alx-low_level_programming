5af89822e410803db4a6d0e8f491dc2565a9dbc0
Fien-Dora <fiendora5@gmail.com>
2022-09-08 19:11:29 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 /**
-  *main - Entry
-  *Reurn: Always 0(Success)
-  */
+ * main - Entry
+ *
+ * Reurn: Always 0(Success)
+ */
 int main(void){
 	puts("Programming is like building a multilingual puzzle");
 	return (0);
