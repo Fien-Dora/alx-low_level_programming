6e09f96af4b3643fac4795e1b174fd564f2f87c5
Fien-Dora <fiendora5@gmail.com>
2022-09-12 03:08:09 -0700

    prints last digit and determines range

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,9 +1,10 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* main - assigns and print the last digit of the random
+/* 
+ *  main - assigns and print the last digit of the random
  * number stored in the variable n each time it is executed
- *Return: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
@@ -11,12 +12,11 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	printf("Last digit of %d is %d", n, n%10);
 	if (n % 10 > 5)
-		printf("and is greater than 5\n");
+		printf("Last digit of %d is %d and is greater than 5\n", n, n % 10
 	else if (n % 10 == 0)
-		printf("and is 0\n");
-	else if (n % 10 <6 && n % 10 != 0)
-		printf("and is less than 6 and not 0\n");
+		printf("Last digit of %d is %d and is 0\n", n, n % 10);
+	else if (n % 10 < :6 && n % 10 != 0)
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
 	return (0);
 }
