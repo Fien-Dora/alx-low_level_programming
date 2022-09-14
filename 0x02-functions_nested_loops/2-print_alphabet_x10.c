dc2a404fa2f5437efdffbf41d2739abd09cee986
Fien-Dora <fiendora5@gmail.com>
2022-09-14 04:21:03 -0700

    alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print the alphabet 10 times and ends with a new line
+ */
+void print_alphabet_x10(void);
+{
+	char i;
+	int j;
+
+	for (j = 1; j <= 10; j++)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			_putchar(i);
+		}
+        	_putchar('\n');
+	}
+}
+
