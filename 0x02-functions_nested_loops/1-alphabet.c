08884069e5d82ffb8be37172d8b6917eb51f04fe
Fien-Dora <fiendora5@gmail.com>
2022-09-14 03:56:00 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - print the alphabet 
+ * and ends with a new line 
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
